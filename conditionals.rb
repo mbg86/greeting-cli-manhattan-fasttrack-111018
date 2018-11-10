7a84c042baf1b2921acd01cabd7281af57b1fde7
Manrique Buso - Garcia <manrique.buso@gmail.com>
2018-11-10 23:56:19 +0000

    automatically backed up by learn

---
+++ conditionals.rb
@@ -0,0 +1,29 @@
+
+#100.times do |n|
+#  puts n + 1
+#end
+
+#x = 0
+#100.times do
+
+#100.times do
+#  counter += 1
+#  if counter % 5 == 0
+#  puts "Buzz"
+#  end
+#end
+
+counter = 0
+while counter < 15
+  counter += 1
+  puts counter
+end
+
+counter = 0
+while counter < 1000
+  puts counter
+  counter += 1
+  if counter == 15
+    break
+  end
+end
